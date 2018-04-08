68d53210a5a41a1af2197756e1bd90cfb22d1955
andreevcode <37613146+andreevcode@users.noreply.github.com>
2018-04-08 13:08:24 +0300

    Added oppotunity to start program with papameters. Some parameteres checks also were added
    Signed-off-by: andreevcode <37613146+andreevcode@users.noreply.github.com>

--- src/ru/andreevcode/soap/NalogCheckAgents.java
+++ src/ru/andreevcode/soap/NalogCheckAgents.java
@@ -6,6 +6,7 @@ import unisoft.ws.fnsndscaws2.request.NdsRequest2;
 import unisoft.ws.fnsndscaws2.request.ObjectFactory;
 import unisoft.ws.fnsndscaws2.response.NdsResponse2;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -15,35 +16,47 @@ import java.util.Scanner;
 public class NalogCheckAgents {
 
 	public static void main(String[] args) {
-		//INN input variable
-		String inn;
-		//KPP input variable
-		String kpp;
-
-		while (true) {
-			Scanner scanner = new Scanner(System.in);
-			System.out.print("Введите ИНН: ");
-			inn = scanner.next();
-			if (inn.length() !=10) {
-				System.out.println("ИНН введен неправильно");
-			}
-			else break;
-		}
-
-		while (true) {
-			Scanner scanner = new Scanner(System.in);
-			System.out.print("Введите КПП: ");
-			kpp = scanner.nextLine();
-			if ((kpp.length() ==9) || (kpp.length()==0)) {
-				break;
-			}
-			else System.out.println("КПП введен неправильно");
-		}
+        //INN input variable
+        String inn = null;
+        //KPP input variable
+        String kpp = null;
+        //Date for request
+        String dateRequest = null;
 
         Date dateNow = new Date();
-        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
-        System.out.println("Текущая дата " + formatForDateNow.format(dateNow));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
 
+        //Start program with arguments. It's possible to make your own data.
+        if (args.length>0)
+        {
+	        if (args.length == 1) inn = args[0];
+            if (args.length >= 2) kpp=args[1];
+            if (inn.length() !=10 || !isValidParamLong(inn))  System.out.println("ИНН введен неправильно");
+            if (kpp.length() !=9 || !isValidParamLong(kpp))  System.out.println("KПП введен неправильно");
+            if (args.length == 3) {
+                dateRequest=getDateRequest(sdf, args[2]);
+            }
+                else dateRequest=sdf.format(dateNow);
+        }
+        else
+        {
+            //INN input
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Введите ИНН: ");
+            if (!scanner.hasNextLong ())  System.out.println("ИНН введен неправильно");
+            inn = scanner.next();
+            if (inn.length() != 10)  System.out.println("ИНН введен неправильно");
+
+            //KPP input
+            System.out.print("Введите КПП: ");
+            if (!scanner.hasNextLong ()) System.out.println("КПП введен неправильно");
+            kpp = scanner.next();
+            if (kpp.length() != 9)  System.out.println("КПП введен неправильно");
+
+            dateRequest=sdf.format(dateNow);
+        }
+
+        System.out.println("Запрос к ЕГРН: " + inn + " " + kpp + " " + dateRequest);
         FNSNDSCAWS2 fnsndscaws2= new FNSNDSCAWS2();
         FNSNDSCAWS2Port fnsndscaws2Port = fnsndscaws2.getFNSNDSCAWS2Port();
 
@@ -51,7 +64,7 @@ public class NalogCheckAgents {
         NdsRequest2.NP np = objectRequest.createNdsRequest2NP();
         np.setINN(inn);
         np.setKPP(kpp);
-        np.setDT(formatForDateNow.format(dateNow));
+        np.setDT(dateRequest);
         NdsRequest2 ndsRequest =  objectRequest.createNdsRequest2();
         List<NdsRequest2.NP> npList = ndsRequest.getNP();
         npList.add(np);
@@ -61,13 +74,40 @@ public class NalogCheckAgents {
 
         //Console output of response main data
         for (NdsResponse2.NP item: responseList) {
-            System.out.println(item.getState() +" "+ item.getINN() + " " + item.getINN() +
+            System.out.println("Ответ ЕГРН: " + item.getState() + " "+ item.getINN() + " " + item.getINN() +
                     " " + item.getDT());
         }
 
         //Console output of response secondary data
-        System.out.println(ndsResponse.getDTActFL() + " " +ndsResponse.getDTActUL() + " "+
+        System.out.println("Доп.данные ответа ЕГРН: " + ndsResponse.getDTActFL() + " " +ndsResponse.getDTActUL() + " "+
                 " " + ndsResponse.getErrMsg());
 	}
+
+	// Date format check
+    private static String getDateRequest(SimpleDateFormat sdf, String value) {
+        Date date = null;
+        String dateRequest=null;
+        try {
+            date = sdf.parse(value);
+            if (value.equals(sdf.format(date))) {
+                dateRequest=value;
+            }
+        } catch (ParseException ex) {
+            System.out.println("Дата введена неправильно");
+        }
+        return dateRequest;
+    }
+
+    //String to int check
+    private static boolean isValidParamLong(String st){
+        boolean flag = true;
+	    try {
+            Long i = Long.parseLong(st);
+        } catch (NumberFormatException e) {
+            flag=false;
+        }
+        return flag;
+    }
+
 
 }
