cf8b66224cf395edf7949adc8df98b12b5232da4
andreevcode <37613146+andreevcode@users.noreply.github.com>
2018-04-08 11:02:49 +0300

    Add logic of pushing NdsRequest2 and getting NdsResponse to console.

--- src/ru/andreevcode/soap/NalogCheckAgents.java
+++ src/ru/andreevcode/soap/NalogCheckAgents.java
@@ -1,5 +1,14 @@
 package ru.andreevcode.soap;
 
+import unisoft.ws.FNSNDSCAWS2;
+import unisoft.ws.FNSNDSCAWS2Port;
+import unisoft.ws.fnsndscaws2.request.NdsRequest2;
+import unisoft.ws.fnsndscaws2.request.ObjectFactory;
+import unisoft.ws.fnsndscaws2.response.NdsResponse2;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 //EGRN agents data check
@@ -30,6 +39,35 @@ public class NalogCheckAgents {
 			}
 			else System.out.println("КПП введен неправильно");
 		}
+
+        Date dateNow = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
+        System.out.println("Текущая дата " + formatForDateNow.format(dateNow));
+
+        FNSNDSCAWS2 fnsndscaws2= new FNSNDSCAWS2();
+        FNSNDSCAWS2Port fnsndscaws2Port = fnsndscaws2.getFNSNDSCAWS2Port();
+
+        ObjectFactory objectRequest = new ObjectFactory();
+        NdsRequest2.NP np = objectRequest.createNdsRequest2NP();
+        np.setINN(inn);
+        np.setKPP(kpp);
+        np.setDT(formatForDateNow.format(dateNow));
+        NdsRequest2 ndsRequest =  objectRequest.createNdsRequest2();
+        List<NdsRequest2.NP> npList = ndsRequest.getNP();
+        npList.add(np);
+
+        NdsResponse2 ndsResponse = fnsndscaws2Port.ndsRequest2(ndsRequest);
+        List<NdsResponse2.NP> responseList =  ndsResponse.getNP();
+
+        //Console output of response main data
+        for (NdsResponse2.NP item: responseList) {
+            System.out.println(item.getState() +" "+ item.getINN() + " " + item.getINN() +
+                    " " + item.getDT());
+        }
+
+        //Console output of response secondary data
+        System.out.println(ndsResponse.getDTActFL() + " " +ndsResponse.getDTActUL() + " "+
+                " " + ndsResponse.getErrMsg());
 	}
 
 }
